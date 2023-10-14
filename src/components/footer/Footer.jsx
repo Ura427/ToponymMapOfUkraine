e072f02bda8dc8a10468f4cea5bdd4aa77a0a66f
Ura427 <urakost427@gmail.com>
2023-10-14 19:28:42 +0300

    v1.01(change navbar, change footer color)

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,12 +1,67 @@
 import React from "react";
 
+import CssBaseline from '@mui/material/CssBaseline';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Link from '@mui/material/Link';
+
+const Copyright = () => {
+  return (
+    <Typography variant="body2" color="text.secondary">
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+// TODO remove, this demo shouldn't need to reset the theme.
+const defaultTheme = createTheme();
+
+
 const Footer = () => {
   return (
-    <footer className="bg-dark py-3 mt-4">
+    <footer className="bg-primary py-3 mt-4">
       <div className="container">
         <p className="text-center text-white">© 2023 Company, Inc</p>
       </div>
     </footer>
+
+    // <ThemeProvider theme={defaultTheme}>
+    //   <Box
+    //     sx={{
+    //       display: 'flex',
+    //       flexDirection: 'column',
+    //       minHeight: '100vh',
+    //     }}
+    //   >
+
+    //     <Box
+    //       component="footer"
+    //       sx={{
+    //         py: 3,
+    //         px: 2,
+    //         mt: 'auto',
+    //         backgroundColor: (theme) =>
+    //           theme.palette.mode === 'light'
+    //             ? theme.palette.grey[200]
+    //             : theme.palette.grey[800],
+    //       }}
+    //     >
+    //       <Container maxWidth="sm">
+    //         <Typography variant="body1">
+    //           My sticky footer can be found here.
+    //         </Typography>
+    //         <Copyright />
+    //       </Container>
+    //     </Box>
+    //   </Box>
+    // </ThemeProvider>
   );
 };
 
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,61 +1,89 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
 const Header = () => {
   return (
-    <header className="p-3 text-bg-dark">
-      <div className="container">
-        <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-          <a
-            href="/"
-            className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none"
+    // <header className="p-3 text-bg-dark">
+    //   <div className="container">
+    //     <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+    //       <a
+    //         href="/"
+    //         className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none"
+    //       >
+    //         <svg
+    //           className="bi me-2"
+    //           width="40"
+    //           height="32"
+    //           role="img"
+    //           aria-label="Bootstrap"
+    //         >
+    //           {/* <use xlink:href="#bootstrap"></use> */}
+    //         </svg>
+    //       </a>
+
+    //       <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+    //         {/* <li><a href="#" className="nav-link px-2 text-secondary">Home</a></li>
+    //             <li><a href="#" className="nav-link px-2 text-white">Features</a></li>
+    //             <li><a href="#" className="nav-link px-2 text-white">Pricing</a></li>
+    //             <li><a href="#" className="nav-link px-2 text-white">FAQs</a></li>
+    //             <li><a href="#" className="nav-link px-2 text-white">About</a></li> */}
+    //       </ul>
+
+    //       <form
+    //         className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3"
+    //         role="search"
+    //       >
+    //         <input
+    //           type="search"
+    //           className="form-control form-control-dark text-bg-dark"
+    //           placeholder="Search..."
+    //           aria-label="Search"
+    //         />
+    //       </form>
+
+    //       <div className="text-end">
+    //         <Link to="/login">
+    //           <button type="button" className="btn btn-outline-light me-2">
+    //             Login
+    //           </button>
+    //         </Link>
+    //         <Link to="/registration">
+    //           <button type="button" className="btn btn-warning">
+    //             Sign-up
+    //           </button>
+    //         </Link>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </header>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
           >
-            <svg
-              className="bi me-2"
-              width="40"
-              height="32"
-              role="img"
-              aria-label="Bootstrap"
-            >
-              {/* <use xlink:href="#bootstrap"></use> */}
-            </svg>
-          </a>
-
-          <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-            {/* <li><a href="#" className="nav-link px-2 text-secondary">Home</a></li>
-                <li><a href="#" className="nav-link px-2 text-white">Features</a></li>
-                <li><a href="#" className="nav-link px-2 text-white">Pricing</a></li>
-                <li><a href="#" className="nav-link px-2 text-white">FAQs</a></li>
-                <li><a href="#" className="nav-link px-2 text-white">About</a></li> */}
-          </ul>
-
-          <form
-            className="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3"
-            role="search"
-          >
-            <input
-              type="search"
-              className="form-control form-control-dark text-bg-dark"
-              placeholder="Search..."
-              aria-label="Search"
-            />
-          </form>
-
-          <div className="text-end">
-            <Link to="/login">
-              <button type="button" className="btn btn-outline-light me-2">
-                Login
-              </button>
-            </Link>
-            <Link to="/registration">
-              <button type="button" className="btn btn-warning">
-                Sign-up
-              </button>
-            </Link>
-          </div>
-        </div>
-      </div>
-    </header>
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Toponym UA
+          </Typography>
+          <Button color="inherit">Login</Button>
+          <Button color="inherit">Sign up</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 };
 
