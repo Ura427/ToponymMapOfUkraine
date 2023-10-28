2616e82efd2377a09342db7f7f5ca0c8dfcb868a
Ura427 <urakost427@gmail.com>
2023-10-29 00:00:47 +0300

    add api retreive

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
-// import "./App.css";
-// import "./myApp.css";
-// import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-
+import React, { useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
-
 
 import Login from "./pages/login/Login";
 import Registration from "./pages/registration/Registration";
 import HomePage from "./pages/homePage/HomePage";
 
 function App() {
+  const [backendData, setBackendData] = useState([]);
+  const [allToponyms, setAllToponyms] = React.useState([]);
+
+  useEffect(() => {
+    fetch("/api/getAll")
+      .then((response) => response.json()) 
+      .then((data) => {
+        setBackendData(data);
+      });
+  }, []);
+
+
+  //   setAllToponyms( backendData.map(region => ({
+  //   regionName: region.name,
+  //   toponyms: region.toponyms.map(toponym => toponym.name)
+  // })))
+
+
+  //   console.log(allToponyms);
   return (
     <div>
+
+      {backendData.map( data => {
+        return <p>{JSON.stringify(data)}</p>
+      })}
+
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<Login />} />
