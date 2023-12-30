b4ab8e6930bf8e32494e57ef294d4361251184f6
Ura427 <urakost427@gmail.com>
2023-12-30 17:12:12 +0200

    move store to separate file

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,13 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 
-import Login from "./components/login/Login";
+import Login from "./components/login/Login.tsx";
 import Registration from "./components/registration/Registration";
 import HomePage from "./components/homePage/HomePage.tsx";
 import AddToponym from "./components/addToponym/AddToponym";
 
-import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "./store/auth.ts";
-import currUserReducer from "./store/currUser.ts";
 
-export const appStore = configureStore({
-  reducer: { auth: authReducer, currUser: currUserReducer },
-});
+
 
 function App() {
   return (
