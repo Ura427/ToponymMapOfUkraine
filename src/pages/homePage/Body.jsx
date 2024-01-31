3667e4a60178f564d0194c93b9666c58cec4c042
Ura427 <urakost427@gmail.com>
2024-01-31 14:39:33 +0200

    moved fetching data to App component and other minor improvements

--- src/pages/homePage/Body.jsx
+++ src/pages/homePage/Body.jsx
@@ -1,12 +1,11 @@
 //Essential and hooks
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import axios from "axios";
 import { Link } from "react-router-dom";
 
 //Styles
 import "./Body.css";
-
 
 //MUI
 import BodySearch from "./BodySearch.jsx";
@@ -19,21 +18,6 @@ import AddIcon from "@mui/icons-material/Add";
 import SvgMap from "../../components/svgMap/SvgMap.jsx";
 import ModalWindow from "../../components/modal/ModalWindow.jsx";
 
-//Redux
-// import { store } from "../../store/store.ts";
-// import {
-//   modalDescActions,
-//   modalOpenActions,
-//   modalStore,
-//   modalTitleActions,
-// } from "../../store/slices/modal.ts";
-// import { avgRatingActions } from "../../store/slices/rating.ts";
-// import { currRatingActions } from "../../store/slices/rating.ts";
-// import { ratingStore } from "../../store/slices/rating.ts";
-// import {
-//   currDataStore,
-//   currToponymActions,
-// } from "../../store/slices/currData.ts";
 
 //Context
 import { RegionContext } from "../../store/context/regionContext.js";
@@ -44,6 +28,12 @@ import { BackendDataContext } from "../../store/context/backendDataContext.js";
 
 //Component
 const Body = () => {
+  const backendData = useContext(BackendDataContext);
+  const toponymsData = useContext(ToponymsDataContext)
+  
+  console.log(backendData.value)
+  console.log(toponymsData.value)
+
   const [currRegion, setCurrRegion] = useState();
   const [currToponym, setCurrToponym] = useState();
   const [modalData, setModalData] = useState({
@@ -56,8 +46,6 @@ const Body = () => {
   const [notProvokeBySearch, setNotProvokeBySearch] = useState(true);
   const isLoggedIn = useSelector((state) => state.auth.value);
 
-  const [backendData, setBackendData] = useState([]);
-  const [toponymsData, setToponymsData] = useState([]);
 
 
   //Set Modal Data
@@ -75,45 +63,10 @@ const Body = () => {
         title: currToponym,
         desc: toponymDescription
       }))
-      // modalStore.dispatch(modalTitleActions.setTitle(currToponym));
-      // modalStore.dispatch(modalDescActions.setDesc(toponymDescription));
     }
   }
 
 
-  //fetch all backend data
-  useEffect(() => {
-    fetch("/api/getAll")
-      .then((response) => response.json())
-      .then((data) => {
-        setBackendData(data);
-      });
-      // backendDataTransform();
-  }, []);
-
-  
-  
-  useEffect(() => {
-    if (backendData.length > 0) {
-      const allToponyms = backendData.flatMap((region) => {
-        return region.toponyms.map((toponym) => ({
-          regionName: region.name,
-          toponymName: toponym.name,
-          toponymDescription: toponym.description,
-        }));
-      });
-
-      setToponymsData(
-        allToponyms.sort((a, b) => {
-          if (a.regionName === b.regionName) {
-            return a.toponymName.localeCompare(b.toponymName);
-          } else {
-            return a.regionName.localeCompare(b.regionName);
-          }
-        })
-      );
-    }
-  })
 
 
   //Gets average toponym rating
@@ -140,20 +93,16 @@ const Body = () => {
 
   //Modal handlers
   function handleOpen() {
-    // modalStore.dispatch(modalOpenActions.setActive());
     setModalData((prevState) => ({
       ...prevState,
       open: true
     }))
   }
-  // function handleClose() {
-  //   modalStore.dispatch(modalOpenActions.setDisabled());
-  //   ratingStore.dispatch(currRatingActions.setCurrRating(0));
-  // }
+
 
   // Database interactions
   function getToponymDescription(region, toponymName) {
-    const toponym = toponymsData.find(
+    const toponym = toponymsData.value.find(
       (element) =>
         element.regionName === region && element.toponymName === toponymName
     );
@@ -164,13 +113,7 @@ const Body = () => {
     return "Toponym not found";
   }
 
-  // //returns a list of toponyms of current region (SHOULD RETURN AN ARRAY OF OBJECTS)
-  // function getInfo(region) {
-  //   const toponyms = toponymsData.filter((element) => {
-  //     return element.regionName === region;
-  //   });
-  //   setToponyms(toponyms);
-  // }
+
 
   //Actions when toponym is clicked inside of a list
   function listItemClickHandler(toponym) {
@@ -196,26 +139,12 @@ const Body = () => {
     setValue: setModalData
   }
 
-  const toponymsDataCtx = {
-    value: toponymsData,
-    setValue: setToponymsData
-  }
-
-  const backendDataContext = {
-    value: backendData,
-    setValue: setBackendData
-  }
 
   return (
     <div id="page-body">
       <RegionContext.Provider value={regionCtx}>
        <ToponymContext.Provider value={toponymCtx}>
-        <ToponymsDataContext.Provider value={toponymsDataCtx}>
         <ModalContext.Provider value={modalCtx}>
-          <BackendDataContext.Provider value={backendDataContext}>
-        
-
-        
         <ModalWindow/>
 
         {currRegion !== "" && toponyms.length > 0 && notProvokeBySearch && (
@@ -228,7 +157,6 @@ const Body = () => {
         <div className="svg-container">
           <SvgMap
             height="650px"
-            // getInfo={getInfo}
             toponymsData={toponymsData}
             setToponyms={setToponyms}
             setNotProvokeBySearch={setNotProvokeBySearch}
@@ -238,7 +166,7 @@ const Body = () => {
       
 
       <BodySearch
-        sortedSearchOptions={toponymsData}
+        sortedSearchOptions={toponymsData.value}
         handleOpen={handleOpen}
         setNotProvokeBySearch={setNotProvokeBySearch}
       />
@@ -256,9 +184,7 @@ const Body = () => {
       )}
 
 
-        </BackendDataContext.Provider>
         </ModalContext.Provider>
-        </ToponymsDataContext.Provider>
       </ToponymContext.Provider> 
     </RegionContext.Provider>
     </div>
